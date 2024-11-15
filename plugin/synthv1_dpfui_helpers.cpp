bcb2f074a490ae32b8c83f5e3b861bbd9601d39e
AnClark <clarklaw4701@qq.com>
2024-11-15 08:17:48 +0800

    (Rebasing) Move from synthv1-universal to synthv1-xt

---
+++ plugin/synthv1_dpfui_helpers.cpp
@@ -0,0 +1,299 @@
+// synthv1_dpfui_helpers.cpp
+//
+/****************************************************************************
+   Copyright (C) 2024, AnClark Liu. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*****************************************************************************/
+
+#include "synthv1_dpfui.h"
+#include "synthv1_param.h"
+
+#include "DearImGuiKnobs/imgui-knobs.h"
+#include "veeone_filter_widget.hpp"
+
+#include "imgui_extentions.h"
+
+// ------------------------------------------------------------------
+// UI Helpers
+
+void SynthV1PluginUI::_addKnob(const char* label, synthv1::ParamIndex paramIndex, bool isZeroMeansOff)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex >= 0 && paramIndex < synthv1::NUM_PARAMS, )
+
+    constexpr float VEEONE_KNOB_SIZE = 40.0f;
+
+    //const char* _format = isZeroMeansOff ? (fParamStorage[paramIndex] == 0 ? "Off" : NULL) : NULL;    // TODO: Does this has better performance? Keep it here.
+    std::string _format = synthv1_param::paramDisplayImGui(paramIndex, fParamStorage[paramIndex], isZeroMeansOff);
+
+    ImGui::BeginGroup();
+
+    // There's an issue with `SameLine` and Groups, see https://github.com/ocornut/imgui/issues/4190.
+    // This is probably not the best solution, but seems to work for now
+    // (Reference: imgui-knobs.cpp)
+    ImGui::GetCurrentWindow()->DC.CurrLineTextBaseOffset = 0;
+
+    // Center the text properly.
+    ImGuiExt::RenderCenteredText(_trimAfterDoubleHash(std::string(label)).c_str(), VEEONE_KNOB_SIZE);
+
+    if (ImGuiKnobs::Knob(label, &fParamStorage[paramIndex], synthv1_param::paramMinValue(paramIndex), synthv1_param::paramMaxValue(paramIndex),
+                        0.0f, _format.c_str(), ImGuiKnobVariant_Tick, VEEONE_KNOB_SIZE, ImGuiKnobFlags_NoTitle | ImGuiKnobFlags_NoInput))
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+        
+        setParameterValue(paramIndex, fParamStorage[paramIndex]);
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    // Origianlly, ImGuiKnobs::Knob provides a drag button to display knob value, and allows users to drag and input new values.
+    // But it is not suitable for synthv1, since the scale of displayed value and actual value are differ.
+    // To avoid misleading users, I replaced the drag button with a simple button.
+    // TODO: Allow users to input new value via a pop-up box.
+    ImGui::Button(_format.append("###").append(std::string(label)).c_str(), ImVec2(VEEONE_KNOB_SIZE, 0));
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addKnobInt(const char* label, synthv1::ParamIndex paramIndex)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex >= 0 && paramIndex < synthv1::NUM_PARAMS, )
+
+    constexpr float VEEONE_KNOB_SIZE = 40.0f;
+
+    std::string _format = synthv1_param::paramDisplayImGui(paramIndex, fParamStorage[paramIndex], false);
+
+    ImGui::BeginGroup();
+
+    // There's an issue with `SameLine` and Groups, see https://github.com/ocornut/imgui/issues/4190.
+    // This is probably not the best solution, but seems to work for now
+    // (Reference: imgui-knobs.cpp)
+    ImGui::GetCurrentWindow()->DC.CurrLineTextBaseOffset = 0;
+
+    // Center the text properly.
+    ImGuiExt::RenderCenteredText(_trimAfterDoubleHash(std::string(label)).c_str(), VEEONE_KNOB_SIZE);
+
+    int param_int_value = int(fParamStorage[paramIndex]);
+
+    if (ImGuiKnobs::KnobInt(label, &param_int_value, synthv1_param::paramMinValue(paramIndex), synthv1_param::paramMaxValue(paramIndex),
+                        0.0f, _format.c_str(), ImGuiKnobVariant_Tick, VEEONE_KNOB_SIZE, ImGuiKnobFlags_NoTitle | ImGuiKnobFlags_NoInput))
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+        
+        float new_value = synthv1_param::paramSafeValue(paramIndex, float(param_int_value));
+        setParameterValue(paramIndex, new_value);
+        parameterChanged(paramIndex, new_value);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    // Origianlly, ImGuiKnobs::Knob provides a drag button to display knob value, and allows users to drag and input new values.
+    // But it is not suitable for synthv1, since the scale of displayed value and actual value are differ.
+    // To avoid misleading users, I replaced the drag button with a simple button.
+    // TODO: Allow users to input new value via a pop-up box.
+    ImGui::Button(_format.append("###").append(std::string(label)).c_str(), ImVec2(VEEONE_KNOB_SIZE, 0));
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addCheckbox(const char* label, synthv1::ParamIndex paramIndex)
+{
+    bool value = fParamStorage[paramIndex] > 0 ? true : false;
+
+    if (ImGui::Checkbox(label, &value))
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        setParameterValue(paramIndex, value ? 1.0f : 0.0f);
+        parameterChanged(paramIndex, value ? 1.0f : 0.0f);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+}
+
+void SynthV1PluginUI::_addToggle_NoLabel(const char* id, synthv1::ParamIndex paramIndex)
+{
+    bool value = fParamStorage[paramIndex] > 0 ? true : false;
+
+    if (ImGui::Toggle(id, &value, ImVec2(0, 20)))
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        setParameterValue(paramIndex, value ? 1.0f : 0.0f);
+        parameterChanged(paramIndex, value ? 1.0f : 0.0f);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+}
+
+void SynthV1PluginUI::_addWaveSelector(const char* label, synthv1::ParamIndex paramIndex)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex == synthv1::DCO1_SHAPE1 || paramIndex == synthv1::DCO1_SHAPE2 
+                                || paramIndex == synthv1::DCO2_SHAPE1 || paramIndex == synthv1::DCO2_SHAPE2
+                                || paramIndex == synthv1::LFO1_SHAPE || paramIndex == synthv1::LFO2_SHAPE, )
+
+    ImGui::BeginGroup();
+
+    int waveform_value = int(fParamStorage[paramIndex]);
+
+    bool s1 = ImGui::RadioButton(std::string("Pulse##").append(label).c_str(), &waveform_value, synthv1_wave_lf::Pulse);
+    bool s2 = ImGui::RadioButton(std::string("Saw##").append(label).c_str(), &waveform_value, synthv1_wave_lf::Saw);
+    bool s3 = ImGui::RadioButton(std::string("Sine##").append(label).c_str(), &waveform_value, synthv1_wave_lf::Sine);
+    bool s4 = ImGui::RadioButton(std::string("Rand##").append(label).c_str(), &waveform_value, synthv1_wave_lf::Rand);
+    bool s5 = ImGui::RadioButton(std::string("Noise##").append(label).c_str(), &waveform_value, synthv1_wave_lf::Noise);
+
+    if (s1 || s2 || s3 || s4 || s5)
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        float new_value = synthv1_param::paramSafeValue(paramIndex, float(waveform_value));
+        setParameterValue(paramIndex, new_value);
+        parameterChanged(paramIndex, new_value);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addFilterTypeSelector(const char* label, synthv1::ParamIndex paramIndex)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex == synthv1::DCF1_TYPE || paramIndex == synthv1::DCF2_TYPE, )
+
+    ImGui::BeginGroup();
+
+    int filter_type_value = int(fParamStorage[paramIndex]);
+
+    bool s1 = ImGui::RadioButton(std::string("LPF##").append(label).c_str(), &filter_type_value, (int)VeeOneWidgets::DcfType::t_LPF);
+    bool s2 = ImGui::RadioButton(std::string("BPF##").append(label).c_str(), &filter_type_value, (int)VeeOneWidgets::DcfType::t_BPF);
+    bool s3 = ImGui::RadioButton(std::string("HPF##").append(label).c_str(), &filter_type_value, (int)VeeOneWidgets::DcfType::t_HPF);
+    bool s4 = ImGui::RadioButton(std::string("BRF##").append(label).c_str(), &filter_type_value, (int)VeeOneWidgets::DcfType::t_BRF);
+
+    if (s1 || s2 || s3 || s4)
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        float new_value = synthv1_param::paramSafeValue(paramIndex, float(filter_type_value));
+        setParameterValue(paramIndex, new_value);
+        parameterChanged(paramIndex, new_value);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addFilterSlopeSelector(const char* label, synthv1::ParamIndex paramIndex)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex == synthv1::DCF1_SLOPE || paramIndex == synthv1::DCF2_SLOPE, )
+
+    ImGui::BeginGroup();
+
+    int slope_type_value = int(fParamStorage[paramIndex]);
+
+    bool s1 = ImGui::RadioButton(std::string("12dB/oct##").append(label).c_str(), &slope_type_value, (int)VeeOneWidgets::FilterSlope::s_12dbPerOct);
+    bool s2 = ImGui::RadioButton(std::string("24dB/oct##").append(label).c_str(), &slope_type_value, (int)VeeOneWidgets::FilterSlope::s_24dBPerOct);
+    bool s3 = ImGui::RadioButton(std::string("Biquad##").append(label).c_str(), &slope_type_value, (int)VeeOneWidgets::FilterSlope::s_biquad);
+    bool s4 = ImGui::RadioButton(std::string("Formant##").append(label).c_str(), &slope_type_value, (int)VeeOneWidgets::FilterSlope::s_formant);
+
+    if (s1 || s2 || s3 || s4)
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        float new_value = synthv1_param::paramSafeValue(paramIndex, float(slope_type_value));
+        setParameterValue(paramIndex, new_value);
+        parameterChanged(paramIndex, new_value);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addKeyboardModeSelector(const char* label, synthv1::ParamIndex paramIndex)
+{
+    DISTRHO_SAFE_ASSERT_RETURN(paramIndex == synthv1::DEF1_MONO || paramIndex == synthv1::DEF2_MONO, )
+
+    ImGui::BeginGroup();
+
+    int keyboard_mode_value = int(fParamStorage[paramIndex]);
+
+    bool s1 = ImGui::RadioButton(std::string("Poly##").append(label).c_str(), &keyboard_mode_value, 0);
+    bool s2 = ImGui::RadioButton(std::string("Mono##").append(label).c_str(), &keyboard_mode_value, 1);
+    bool s3 = ImGui::RadioButton(std::string("Legato##").append(label).c_str(), &keyboard_mode_value, 2);
+
+    if (s1 || s2 || s3)
+    {
+        if (ImGui::IsItemActivated())
+            editParameter(paramIndex, true);
+
+        float new_value = synthv1_param::paramSafeValue(paramIndex, float(keyboard_mode_value));
+        setParameterValue(paramIndex, new_value);
+        parameterChanged(paramIndex, new_value);    // Trigger param storage update & repaint
+    }
+
+    if (ImGui::IsItemDeactivated())
+    {
+        editParameter(paramIndex, false);
+    }
+
+    ImGui::EndGroup();
+}
+
+void SynthV1PluginUI::_addViewSwitchButton(const char* label, View viewIndex)
+{
+    if (fCurrentView == viewIndex) {
+        ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(7.0f, 0.6f, 0.6f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(7.0f, 0.7f, 0.7f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(7.0f, 0.8f, 0.8f));
+    }
+
+    if (ImGui::Button(label))
+        fCurrentView = viewIndex;
+
+    if (fCurrentView == viewIndex)
+        ImGui::PopStyleColor(3);
+}
+
+// end of synthv1_dpfui_helpers.cpp
---
+++ plugin/synthv1_dpfui_utils.cpp
@@ -0,0 +1,134 @@
+#include "synthv1_config.h"
+#include "synthv1_dpfui.h"
+#include "synthv1_param.h"
+
+#include <random>
+
+// For get_system_config_dir()
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+void SynthV1PluginUI::_randomParams()
+{
+	float p = 1.0f;
+
+	synthv1_config *pConfig = synthv1_config::getInstance();
+	if (pConfig)
+		p = 0.01f * pConfig->fRandomizePercent;
+
+#if 0
+	if (QMessageBox::warning(this,
+		tr("Warning"),
+		tr("About to randomize current parameter values:\n\n"
+		"-/+ %1%.\n\n"
+		"Are you sure?").arg(100.0f * p),
+		QMessageBox::Ok | QMessageBox::Cancel) == QMessageBox::Cancel)
+		return;
+#endif
+
+	std::default_random_engine re(::time(nullptr));
+
+	for (uint32_t i = 0; i < synthv1::NUM_PARAMS; ++i) {
+		const synthv1::ParamIndex index = synthv1::ParamIndex(i);
+		// Filter out some non-randomizable parameters!...
+		if (index == synthv1::DCO1_OCTAVE  ||
+			index == synthv1::DCO1_TUNING  ||
+			index == synthv1::DCO1_ENVTIME ||
+			index == synthv1::DCF1_ENABLED ||
+			index == synthv1::LFO1_ENABLED) 
+			continue;
+		if (index >= synthv1::OUT1_WIDTH && index < synthv1::DCO2_SHAPE1)
+			continue;
+		if (index == synthv1::DCO2_OCTAVE  ||
+			index == synthv1::DCO2_TUNING  ||
+			index == synthv1::DCO2_ENVTIME ||
+			index == synthv1::DCF2_ENABLED ||
+			index == synthv1::LFO2_ENABLED) 
+			continue;
+		if (index >= synthv1::OUT2_WIDTH)
+			break;
+
+		{
+			std::normal_distribution<float> nd;
+			const float q = p * (synthv1_param::paramMaxValue(index) - synthv1_param::paramMinValue(index));
+			float fValue = fParamStorage[index];
+
+			if (synthv1_param::paramFloat(index))
+				fValue += 0.5f * q * nd(re);
+			else
+				fValue = std::round(fValue + q * nd(re));
+			if (fValue < synthv1_param::paramMinValue(index))
+				fValue = synthv1_param::paramMinValue(index);
+			else if (fValue > synthv1_param::paramMaxValue(index))
+				fValue = synthv1_param::paramMaxValue(index);
+
+			setParameterValue(index, fValue);
+			parameterChanged(index, fValue);
+		}
+	}
+}
+
+void SynthV1PluginUI::_panic()
+{
+	// In official synthv1, UI directly invokes DSP's method to trigger a panic (synthv1::reset()).
+	//
+	// According to DPF's policy, it's not recommended to let the UI side directly access DSP side.
+	// So a better way is to send a MIDI All Notes Off event, and invoke panic when detected the event.
+	//
+	// I will check and handle the event on synthv1_dpf::run().
+
+	/**
+	  SPECIAL NOTE ABOUT DISTRHO::UI::sendNote()
+
+	  There is no API to send MIDI data to DSP side. Instead, DPF provides UI::sendNote().
+	  But we can still send any MIDI data via this method, simply doing a hack.
+
+	  In method UI::sendNote(uint8_t channel, uint8_t note, uint8_t velocity):
+
+	  `channel` will be converted into MIDI data 0 by:
+		`midi_data[0] = (velocity != 0 ? 0x90 : 0x80) | channel;`
+
+	  To send MIDI controller data, we need to transform midi_data[0] into MIDI_STATUS_CONTROLLER (0xB0)
+	  by simply applying a proper `channel` value. It would be easy to calculate:
+			0x80 | channel = 0xB0
+		=> 0b10000000 | channel = 0b10110000
+		=> channel = 0b00110000
+		=> channel = 0x30
+	
+	  Similarily, to send SysEx data, just do this transformation:
+	       0x80 | channel = 0xF0
+		=> 0b10000000 | channel = 0b11110000
+		=> channel = 0b11110000
+		=> channel = 0xf0
+	*/
+	sendNote(0xF0, 0x7B, 0);  //I defined SysEx "F0 7B" as Panic command. "7B" is the same as MIDI_ALL_NOTES_OFF.
+}
+
+
+// --------------------------------------------------------------------------
+// Global functions.
+
+DISTRHO::String get_system_config_dir() {
+#ifdef _WIN32
+    // Windows
+    char appdata[1024];
+    DWORD size = sizeof(appdata);
+    GetEnvironmentVariable("APPDATA", appdata, size);
+    return DISTRHO::String(appdata);
+#else
+    // POSIX systems
+    const char* config_home = getenv("XDG_CONFIG_HOME");
+    if (config_home == nullptr) {
+        // Fallback to ~/.config if XDG_CONFIG_HOME is not set
+        char* home = getenv("HOME");
+        if (home) {
+            return DISTRHO::String(home) + "/.config";
+        }
+    }
+    return DISTRHO::String(config_home);
+#endif
+}
